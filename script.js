aa9436e86a36d16f12cf0979ba8bb762677c04a5
ephraim_omenai <eomenaiofficial@gmail.com>
2021-04-08 23:52:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+
+navUL.classList.toggle('show');
+
+});
